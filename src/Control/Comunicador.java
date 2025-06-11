0763f82072d90750cda5b5161bf4ba3db101f71f
Victor Hugo <Vhomoutlook@gmail.com>
2025-06-11 10:00:41 -0300

    mudanças

--- src/Control/Comunicador.java
+++ src/Control/Comunicador.java
@@ -1,7 +1,10 @@
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
-public class ChatClient {
+public class Comunicador {
     public static void main(String[] args) {
         try (
             Socket socket = new Socket("localhost", 12345);
